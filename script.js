f5ee0bc1c828d1a864a18345cc184b4545a1ac1f
Gaurav Soni <120136122+gaurav8158@users.noreply.github.com>
2023-12-15 03:28:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	let factorial = fact(n);
+	alert(factorial) 
+	let num=0;
+	while(factorial%10===0){
+		num+=1;
+		factorial=factorial/10;
+	}
+	return num;
+	
+}
+function fact(n){
+	if(n===0 || n===1){
+		return 1;
+	}
+	else{
+return n*fact(n-1);}  
 }
 
-const input = prompt("Enter a number");
+const input = prompt("Enter a number"); 
 alert(trailingZeros(input));
